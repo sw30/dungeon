81be40438afa2336410c546367f27b84a1f39f0c
Urszula Sołogub <gold.ula4@gmail.com>
2020-01-11 22:14:50 +0100

    init

---
+++ core/src/com/gdx/dungeon/DungeonClient.java
@@ -0,0 +1,47 @@
+package com.gdx.dungeon;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
+public class DungeonClient extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	private Socket socket;
+	private String URI = "http://localhost:8080";
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		connectSocket();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+
+	public void connectSocket() {
+		try {
+			socket = IO.socket(URI);
+			socket.connect();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ desktop/src/com/gdx/dungeon/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.gdx.dungeon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdx.dungeon.DungeonClient;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DungeonClient(), config);
+
+	}
+}
